f762baae30f4561f962c11f1ae74296ce0c4f13e
kjh <lykim0418@naver.com>
2023-02-05 21:19:13 +0900

    #21 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    나머지 로직 테스트 코드를 작성하고 연관된 DTO를 재설계함
    테스트를 필요로 하는 repository 쿼리 메소드도 작성

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -47,7 +47,7 @@ public class Article extends AuditingFields{
     @ToString.Exclude
     @OrderBy("createdAt DESC")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
-    private final Set<ArticleComment> articleCommentSet = new LinkedHashSet<>();
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
     protected Article(){}
 
